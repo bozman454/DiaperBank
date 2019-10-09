a6742fa95f61725d4e20c6dec6c6bbd3d3c8714f
ChristinaEd <C.Edmunds0@gmail.com>
2019-10-09 16:40:53 -0400

    Added childinfo component, child form

---
+++ DiaperBankFront/src/app/childinfo/childinfo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-childinfo',
+  templateUrl: './childinfo.component.html',
+  styleUrls: ['./childinfo.component.css']
+})
+export class ChildinfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
