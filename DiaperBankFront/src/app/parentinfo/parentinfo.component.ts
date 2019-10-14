d9d744f32100cd5f49a15ae0e7829b7d18e241ca
ChristinaEd <C.Edmunds0@gmail.com>
2019-10-14 14:10:23 -0400

    Restyled child list, add confirmation for deletion

--- DiaperBankFront/src/app/parentinfo/parentinfo.component.ts
+++ DiaperBankFront/src/app/parentinfo/parentinfo.component.ts
@@ -26,8 +26,24 @@ export class ParentinfoComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.parentForm.get('parentPhone').setValue('');
   }
 
+  phoneFormat(str, key) {
+    /*console.log("str: " + str)
+    console.log("key: " + key)
+
+    if (key.match("[0-9]")) {
+
+    } else if (key.match("[(]") | key.match("[)]")) {
+      console.log("paren")  
+    } else if (key.match("[A-Za-z]")) {
+      //console.log(str.slice(0, str.length - 1))
+      this.parentForm.get('parentPhone').setValue(str.slice(0, str.length - 1))
+    }*/
+
+    //this.parentForm.get('parentPhone').setValue(str.replace('/^([0-9])/gi', ''))
+  }
 
   submit(first, last, address, city, state, zip, county, phone){
     console.log('Component: ' + first, last, address, city, state, zip, county, phone)
