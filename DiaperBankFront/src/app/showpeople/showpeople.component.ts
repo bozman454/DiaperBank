a3e9279dbaa581c8ee8b312b8fa98d2f279e4a8d
djn3811 <normandonalde@gmail.com>
2019-10-15 10:19:38 -0400

    Show People table

--- DiaperBankFront/src/app/showpeople/showpeople.component.ts
+++ DiaperBankFront/src/app/showpeople/showpeople.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RegisterService } from '../register.service';
 
 @Component({
   selector: 'app-showpeople',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./showpeople.component.css']
 })
 export class ShowpeopleComponent implements OnInit {
+  patronList;
 
-  constructor() { }
+  constructor(public register: RegisterService) { }
 
   ngOnInit() {
   }
 
+
+    //Calls the service to get the already scanned people and subscribes to the returned record
+    showPeople(){
+      this.register.getPreRegistered()
+      .subscribe(record => {
+        this.patronList = record;
+        // console.log('showPeople(): ' + this.patronList)
+      })
+    }
 }
